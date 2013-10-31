aca04b643c2a1df1e1e75c4b50f611420e261698
Buzachis Aris <buzachis.aris@gmail.com>
2013-11-01 01:07:06 +0200

    Added routes constraint for ip whitelisting.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
-require 'ka_ext_users/constraints/ip_whitelist_constraint'
+module KaExtUsers
+  class IpWhitelistConstraint
+    def initialize
+      @ips = []
+      #@ips << ::IPAddr.new('127.0.0.0')
+      # you can also add blocks of IPs
+      # @ips << ::IPAddr.new('192.168.0.0/16')
+    end
+  
+    def matches?(request)
+      valid = @ips.select { |cidr| cidr===request.remote_ip }
+      !valid.empty?
+    end
+  end
+end
 
 KaExtUsers::Engine.routes.draw do
   constraint KaExtUsers::IpWhitelistConstraint.new do
