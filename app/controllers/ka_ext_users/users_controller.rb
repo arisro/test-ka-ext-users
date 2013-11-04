14ff47fce8c9d775a295124626d585a3fefc9833
Aris BUZACHIS <aris-antonio.buzachis@ubisoft.com>
2013-11-04 11:54:14 +0200

    ...

--- app/controllers/ka_ext_users/users_controller.rb
+++ app/controllers/ka_ext_users/users_controller.rb
@@ -10,7 +10,7 @@ module KaExtUsers
 
     def show  
         @user = User.find(params[:id])  	
-        respond_with @user, :represent_with => KaExtUsers::UserRepresenter
+        respond_with @user
 
         # respond_with(@user) do |format|
         #     format.json { render }
