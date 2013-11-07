a6fff53def0606a09720ea024caf7a9db266fc10
Aris BUZACHIS <buzachis.aris@gmail.com>
2013-11-07 19:47:49 +0000

    arghhh

--- lib/ka_ext_users/ext_database.rb
+++ lib/ka_ext_users/ext_database.rb
@@ -1,7 +1,6 @@
 module KaExtUsers
 	class ExtDatabase < ActiveRecord::Base
 		self.abstract_class = true
-    puts KaExtUsers::Engine.database_config.inspect
 		establish_connection KaExtUsers::Engine.database_config unless KaExtUsers::Engine.database_config.nil?
 	end
 end
